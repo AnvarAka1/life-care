04e963484a10ccaa5cb4b020418805f59fab669d
Anvar <Anvar-abd_97@mail.ru>
2021-03-08 22:59:16 +0500

    lang dropdown

--- public/js/main.js
+++ public/js/main.js
@@ -1,50 +1,48 @@
 
-    ///////////////////////////////
-    // 08. Case Slider Js
-    $('.case__slider--active').slick({
-        centerMode: true,
-        centerPadding: '0px',
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: false,
-        responsive: [{
-                breakpoint: 767,
-                settings: {
-                    dots: false,
-                    arrows: false,
-                    centerMode: true,
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
+/// ////////////////////////////
+// 08. Case Slider Js
+$('.case__slider--active').slick({
+  centerMode: true,
+  centerPadding: '0px',
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: false,
+  responsive: [{
+    breakpoint: 767,
+    settings: {
+      dots: false,
+      arrows: false,
+      centerMode: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
 
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: false,
-                    arrows: false,
-                    centerMode: true,
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                }
-            }, {
-                breakpoint: 1024,
-                settings: {
-                    dots: false,
-                    arrows: false,
-                    centerMode: true,
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                }
-            }
-        ]
-    });
+    }
+  },
+  {
+    breakpoint: 992,
+    settings: {
+      dots: false,
+      arrows: false,
+      centerMode: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+    }
+  }, {
+    breakpoint: 1024,
+    settings: {
+      dots: false,
+      arrows: false,
+      centerMode: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+    }
+  }
+  ]
+})
 
-
-
-
-
-
-
-   
+// $(function () {
+//   $('header.mobile-header nav.mobile-menu').meanmenu({
+//     meanScreenWidth: '800'
+//   })
+// })

16cf3e8ebf17477f493e80c966000754b498b1ad
Anvar <Anvar-abd_97@mail.ru>
2021-03-09 11:05:51 +0500

    Promise.all

--- controllers/index.js
+++ controllers/index.js
@@ -9,46 +9,45 @@ const Treatment = require('../models/treatment')
 const { getFormattedItem } = require('../utils/item')
 const { getFormattedList } = require('../utils/list')
 
+const getSlideshows = () => Slideshow.find({}).limit(10).lean()
+const getAdvantage = () => Advantage.findOne().lean()
+const getServices = () => Service.find({}).limit(6).lean()
+const getTreatments = () => Treatment.find({}).limit(6).lean()
+const getAchievement = () => Achievement.findOne().lean()
+const getTestimonial = () => Testimonial.find({}).limit(10).lean()
+const getBlogs = () => Blog.find({}).sort({ _id: 'desc' }).lean().limit(3)
+const getPartners = () => Partner.find({}).lean()
+
 exports.getIndex = (req, res, next) => {
-  let slideshowItems = null
-  let advantage = null
-  let services = null
-  let treatments = null
-  let achievement = null
-  let testimonials = null
-  let blogs = null
-  let partners = null
-  return Slideshow.find({}).limit(10).lean()
-    .then(results => {
-      slideshowItems = getFormattedList(req.language, results)
-      return Advantage.findOne().lean()
-    })
-    .then(result => {
-      advantage = getFormattedItem(req.language, result)
-      return Service.find({}).limit(6).lean()
-    })
-    .then(results => {
-      services = getFormattedList(req.language, results)
-      return Treatment.find({}).limit(6).lean()
-    })
-    .then(results => {
-      treatments = getFormattedList(req.language, results)
-      return Achievement.findOne().lean()
-    })
-    .then(result => {
-      achievement = getFormattedItem(req.language, result)
-      return Testimonial.find({}).limit(10).lean()
-    })
-    .then(results => {
-      testimonials = getFormattedList(req.language, results)
-      return Blog.find({}).sort({ _id: 'desc' }).lean().limit(3)
-    })
-    .then(results => {
-      blogs = getFormattedList(req.language, results)
-      return Partner.find({}).lean()
-    })
-    .then(results => {
-      partners = getFormattedList(req.language, results)
+  return Promise.all([
+    getSlideshows(),
+    getAdvantage(),
+    getServices(),
+    getTreatments(),
+    getAchievement(),
+    getTestimonial(),
+    getBlogs(),
+    getPartners()
+  ])
+    .then(([
+      slideshows,
+      advantageItems,
+      serviceItems,
+      treatmentItems,
+      achievementItem,
+      testimonialItems,
+      blogItems,
+      partnersItems
+    ]) => {
+      const slideshowItems = getFormattedList(req.language, slideshows)
+      const advantage = getFormattedItem(req.language, advantageItems)
+      const services = getFormattedList(req.language, serviceItems)
+      const treatments = getFormattedList(req.language, treatmentItems)
+      const achievement = getFormattedItem(req.language, achievementItem)
+      const testimonials = getFormattedList(req.language, testimonialItems)
+      const blogs = getFormattedList(req.language, blogItems)
+      const partners = getFormattedList(req.language, partnersItems)
+
       return res.render('index', {
         slideshowItems,
         advantage,
